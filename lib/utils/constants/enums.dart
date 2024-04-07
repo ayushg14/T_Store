7912e95a7f16124b946f0af16b5246c575b591f6
Ayush Gupta <ayushg1408@gmail.com>
2024-04-07 18:22:23 +0530

    Basic File Strucure of App Dev

---
+++ lib/utils/constants/enums.dart
@@ -0,0 +1,17 @@
+// They cannot be created inside a class.
+
+enum TextSizes { small, medium, large }
+
+enum OrderStatus { processing, shiped, delivered }
+
+enum PaymentMethods {
+  paypal,
+  googlePay,
+  applePay,
+  visa,
+  masterCard,
+  creditCard,
+  paystack,
+  razorpay,
+  paytm
+}
---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,9 @@
+class TImages {
+  // App Logos
+  static const String darkAppLogo = "assets/logos";
+  static const String lightAppLogo = "assts/";
+
+  // Social Logos
+  static const String google = "/assets/logos/google-icons.png";
+  static const String facebook = "/assets/logos/facebook-icon-512x512-seb542ju-removebg-preview.png";
+}
